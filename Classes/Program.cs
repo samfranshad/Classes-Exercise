18fa4d889d70b2f0521ab66e6487c567daf97802
Sam Shadwell <sshadwell978@gmail.com>
2024-05-08 11:51:44 -0400

    Forked and cloned, completed exercise

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -4,7 +4,13 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var carOne = new Cars();
+
+            carOne.Make = "Ford";
+            carOne.Model = "F-150";
+            carOne.Year = 2004;
+
+            Console.WriteLine($"My first car was a {carOne.Year} {carOne.Make} {carOne.Model}");
         }
     }
 }
